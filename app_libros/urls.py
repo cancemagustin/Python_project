7a01772c99e20b6900614109d7b7e3297af48289
Agustin Cancelare <agusmaita32@gmail.com>
2024-12-26 00:53:18 -0300

    realizado el crud

--- app_libros/urls.py
+++ app_libros/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
-
 from . import views
 
 app_name = "blog"
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('post_list', views.post_list, name='post_list' ),
-    path('post_create', views.post_create, name="post_create"),
+    #path('post_list', views.post_list, name='post_list' ),
+    path('post/list', views.PostListView.as_view(), name='post_list'),
+    path('post/create', views.post_create, name="post_create"),
+    path('post/update/<int:pk>', views.actualizar_post, name="post_update"),
+    path('post/detail/<int:pk>', views.PostDetailView.as_view(), name="post_detail"),
+    path('post/delete/<int:pk>', views.eliminar, name="post_delete"),
+    #path('post/delete/<int:id>', views.PostDeleteView.as_view(), name="post_delete"),
+
 ]
--- app_libros/views.py
+++ app_libros/views.py
@@ -1,12 +1,60 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
 from .models import Post
 from .forms import PostForm
-
+from django.views.generic import ListView, DetailView, DeleteView
 # Create your views here.
 
-def post_list(request):
-    post_list = Post.objects.all()
-    return render(request, 'app_libros/post_list.html', context={"posts": post_list})
+
+
+def actualizar_post(request, pk):
+    post = Post.objects.get(id=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect("blog:post_list")
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'app_libros/post_update.html', context={"form": form, "post": post})
+
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = "app_libros/post_list.html"
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        busqueda = self.request.GET.get("busqueda", None)
+        if busqueda:
+            queryset = queryset.filter(titulo__icontains=busqueda)
+        return queryset
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+
+"""
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = reverse_lazy('blog:post_list')
+
+
+"""
+def eliminar(request,pk):
+    post = get_object_or_404(Post, id=pk)
+    if request.method=="POST":
+        post.delete()
+        return redirect("blog:post_list")
+    contexto = {"post":post}
+    return render(request, 'app_libros/post_confirm_delete.html', contexto)
+
+
 
 def post_create(request):
     if request.method == "POST":
